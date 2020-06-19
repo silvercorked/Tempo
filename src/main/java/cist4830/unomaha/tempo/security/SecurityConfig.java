7875214bda763992ec5ad741af28a78aa715f729
Alex Wissing <awissing123@gmail.com>
2020-06-18 21:39:45 -0500

    set up the db configuration and made 2 tables.

---
+++ src/main/java/cist4830/unomaha/tempo/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package cist4830.unomaha.tempo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/", "/home").permitAll()
+        .antMatchers("/admin", "/h2_console/**").permitAll();
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+  }
+}
---
+++ src/main/resources/db/migration/V2__create_goal_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE goals (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  goal varchar(100) NOT NULL,
+  description varchar(1000) NOT NULL,
+  PRIMARY KEY (id)
+);
