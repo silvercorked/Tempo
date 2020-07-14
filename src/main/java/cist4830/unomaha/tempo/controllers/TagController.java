1445befb476b8a2c310d060a85cdbf43a352172a
Cole Maly <malycole@gmail.com>
2020-07-14 12:39:13 -0500

    Update TagController.java

--- src/main/java/cist4830/unomaha/tempo/controllers/TagController.java
+++ src/main/java/cist4830/unomaha/tempo/controllers/TagController.java
@@ -4,13 +4,17 @@ import java.util.Optional;
 
 import cist4830.unomaha.tempo.model.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import cist4830.unomaha.tempo.repository.GoalRepository;
 import cist4830.unomaha.tempo.repository.TagRepository;
 import cist4830.unomaha.tempo.repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value = "tags")
 public class TagController {
     @Autowired
     private UserRepository userRepository;
@@ -33,5 +37,10 @@ public class TagController {
         model.addAttribute("tags", tagRepository.findAll());
         return "tags/index";
     }
+
+    @GetMapping(value = "/create")
+    public String create() { return "goals/add"; }
+
+
 
 }
