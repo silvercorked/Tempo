a9218f9c759e0e1a56053761e2819437130c1b28
Cole Maly <malycole@gmail.com>
2020-07-14 12:24:23 -0500

    Created tag page and controller. not yet functional.

---
+++ src/main/java/cist4830/unomaha/tempo/controllers/TagController.java
@@ -0,0 +1,37 @@
+package cist4830.unomaha.tempo.controllers;
+
+import java.util.Optional;
+
+import cist4830.unomaha.tempo.model.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cist4830.unomaha.tempo.repository.GoalRepository;
+import cist4830.unomaha.tempo.repository.TagRepository;
+import cist4830.unomaha.tempo.repository.UserRepository;
+
+public class TagController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
+    private GoalRepository goalRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        System.out.println("attempted to hit tags page");
+        Optional<Tag> possibleTag = tagRepository.findById((long) 1);
+        Tag tag = possibleTag.orElseGet(() -> {
+           Tag tempTag = new Tag((long) 2, "Test Tag", "Designed to test initial tags",
+                   (long) 1, "2020-10-10 12:12:12", "2020-10-10 12:12:12" );
+           return tagRepository.create(tempTag);
+        });
+        model.addAttribute("tags", tagRepository.findAll());
+        return "tags/index";
+    }
+
+}
