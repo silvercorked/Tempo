e7e47dd1da5671f847a6628fdd31d6ffd0ade4f5
Alex Wissing <awissing123@gmail.com>
2020-07-14 16:59:02 -0500

    setup backend system for associating goals and tags

---
+++ src/main/java/cist4830/unomaha/tempo/model/GoalTagAssoc.java
@@ -0,0 +1,40 @@
+package cist4830.unomaha.tempo.model;
+
+public class GoalTagAssoc {
+	
+	private Long id;
+	private Long goalId;
+	private Long tagId;
+	private String created_at;
+
+	public GoalTagAssoc(Long id, Long goalId, Long tagId, String created_at) {
+		this.setId(id);
+		this.setGoalId(goalId);
+		this.setTagId(tagId);
+		this.setCreatedAt(created_at);
+	}
+	public Long getId() {
+		return this.id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getGoalId() {
+		return this.goalId;
+	}
+	public void setGoalId(Long goalId) {
+		this.goalId = goalId;
+	}
+	public Long getTagId() {
+		return this.tagId;
+	}
+	public void setTagId(Long tagId) {
+		this.tagId = tagId;
+	}
+	public String getCreatedAt() {
+		return this.created_at;
+	}
+	public void setCreatedAt(String created_at) {
+		this.created_at = created_at;
+	}
+}
